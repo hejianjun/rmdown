da7557f3ce1a2dbb6850a721121edc605ad732ee
hejianjun <942156265@qq.com>
2018-04-05 18:13:18 +0800

    添加多进程和进度条

--- rmdown.py
+++ rmdown.py
@@ -1,36 +1,49 @@
-import json
-import requests
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import json,os,requests,progressbar,logging
 from lxml import etree
-import os
-import progressbar
-import logging
+from multiprocessing import Pool
+#import pdb
 
+max_thread_num = 4
 json_file = 'YaZhouWuMa.jsonlines'
 url = 'http://www.rmdown.com/download.php'
-root_path ='E:\\images\\'
+root_path ='E:\\torrent\\'
 
 def load(json_file,callback):
-    bar = progressbar.ProgressBar()
-    for line in bar(open(json_file, 'r')):
-        data = json.loads(line)
-        if data.has_key('t_title') and data.has_key('file_urls'):
-            for file_url in data["file_urls"]:
-                payload = get_payload(file_url);
-                if payload.has_key('ref') and payload.has_key('reff'):
-                    path=file_path(data["t_title"],payload["ref"])
-                    callback(path,payload)
+    fsize = os.path.getsize(json_file)
+    bar = progressbar.ProgressBar(max_value=fsize)
+    p = Pool(max_thread_num)
+    with open(json_file, 'r') as file:
+        for line in file:
+            #pdb.set_trace()
+            try:
+                data = json.loads(line)
+                if data.has_key('t_title') and data.has_key('file_urls'):
+                    folder = file_folder(data["t_title"])
+                    logging.info(folder)
+                    for file_url in data["file_urls"]:
+                        payload = get_payload(file_url);
+                        if payload.has_key('ref') and payload.has_key('reff'):
+                            path = '%s/%s.torrent' % (folder,payload["ref"])
+                            p.apply_async(callback,args=(path,payload))
+            except Exception,e:
+                logging.error(e)
+            i=file.tell()
+            bar.update(i)
+    p.close()
+    p.join()
 
-def file_path(title,ref):
+def file_folder(title):
     if len(title):
         title = title[0]
         table = dict((ord(char), None) for char in "|\\?*<\":>+[]/'")
         title = title.translate(table)
     else:
-        title = u'ttt'
+        title = u'test'
     if not os.path.exists(root_path+title):
         os.makedirs(root_path+title)
-    logging.error('Got %d', title)
-    return '%s/%s.torrent' % (title,ref)
+    return title
 
 def get_payload(url):
     r = requests.get(url)
@@ -47,4 +60,6 @@ def download(path,payload):
         decode.write(r.content)
 
 if __name__ == '__main__':
+    progressbar.streams.wrap_stderr()
+    logging.basicConfig()
     load(json_file,download)
--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import time
 import progressbar
+import os
+import logging
 
-bar = progressbar.ProgressBar(redirect_stdout=True)
-for i in range(100):
-    print 'Some text', i
-    time.sleep(0.1)
+progressbar.streams.wrap_stderr()
+logging.basicConfig()
+
+json_file = 'YaZhouWuMa.jsonlines'
+
+fsize = os.path.getsize(json_file)
+bar = progressbar.ProgressBar(max_value=fsize)
+file = open(json_file, 'r')
+for line in file:
+    time.sleep(0.001)
+    i=file.tell()
     bar.update(i)
