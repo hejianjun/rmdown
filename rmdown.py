ae33fefbb79b15a3e53bfc7c1e2f9cf2bd85480c
hejianjun <942156265@qq.com>
2018-04-05 10:17:26 +0800

    1

---
+++ rmdown.py
@@ -0,0 +1,53 @@
+import json
+import requests
+from lxml import etree
+import os
+import progressbar
+import logging
+
+json_file = 'YaZhouWuMa.jsonlines'
+url = 'http://www.rmdown.com/download.php'
+root_path ='E:\\images\\'
+
+def load(json_file,callback):
+    callback.next()
+    bar = progressbar.ProgressBar()
+    for line in bar(open(json_file, 'r')):
+        data = json.loads(line)
+        if data.has_key('t_title') and data.has_key('file_urls'):
+            for file_url in data["file_urls"]:
+                payload = get_payload(file_url);
+                if payload.has_key('ref') and payload.has_key('reff'):
+                    path=file_path(data["t_title"],payload["ref"])
+                    callback.send(path,payload)
+    callback.close()
+
+def file_path(title,ref):
+    if len(title):
+        title = title[0]
+        table = dict((ord(char), None) for char in "|\\?*<\":>+[]/'")
+        title = title.translate(table)
+    else:
+        title = u'ttt'
+    if not os.path.exists(root_path+title):
+        os.makedirs(root_path+title)
+    logging.info('Got %s', title)
+    return '%s/%s.torrent' % (title,ref)
+
+def get_payload(url):
+    r = requests.get(url)
+    selector = etree.HTML(r.text)
+    result = selector.xpath('//input')
+    dict = {}
+    for input in result:
+        dict[input.get("name")]=input.get("value")
+    return dict
+
+def download(path,payload):
+    r = requests.get(url,payload)
+    with open(root_path+path, "wb") as decode:
+        decode.write(r.content)
+
+if __name__ == '__main__':
+    d=download()
+    load(json_file,d)
---
+++ test.py
@@ -0,0 +1,8 @@
+import time
+import progressbar
+
+bar = progressbar.ProgressBar(redirect_stdout=True)
+for i in range(100):
+    print 'Some text', i
+    time.sleep(0.1)
+    bar.update(i)
